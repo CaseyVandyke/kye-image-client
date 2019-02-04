12b41b4db656d2825182f66fb27cc18f23268298
Cryosith <caseyvandyke@ymail.com>
2019-02-04 10:40:10 -0700

    heroku config variable

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ||
-  "https://aqueous-peak-48000.herokuapp.com/";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:4000/api";
+//https://aqueous-peak-48000.herokuapp.com/
